14c8c87f64b2c1876302b7912c56e41b10f26a0a
YH <doki2099@gmail.com>
2023-05-30 00:12:07 +0800

    add nostr watcher web

--- pkg/app/server/route.go
+++ pkg/app/server/route.go
@@ -16,7 +16,7 @@ func Router() *gin.Engine {
 
 	router.LoadHTMLGlob("web/templates/*")
 
-	//router.GET("/", SocketHandler)
+	router.GET("/", SocketHandler)
 	router.GET("/watcher", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "watcher.html", gin.H{
 			"title": "Posts",
